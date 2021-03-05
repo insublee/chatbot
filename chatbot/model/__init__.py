36ace6817a35585d0cbfc2d2a7c627b2e67d6ef2
insublee <39117829+insublee@users.noreply.github.com>
2021-03-05 15:18:35 +0900

    Update __init__.py

--- chatbot/model/__init__.py
+++ chatbot/model/__init__.py
@@ -1,6 +1,1 @@
 from .chatbot import Chatbot
-#from .encoder import CustomEncoder
-#from .ltm import LongTermMemory
-#from .selector import Selector
-#from .decoder import CustomDecoder
-#from .sentiment import SentimentAstimater
