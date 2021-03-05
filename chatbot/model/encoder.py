ffd8223d48eb463f42b77053c324c454c58276b9
insubLee <643jason@gmail.com>
2021-03-05 15:05:28 +0900

    bart encoder

--- chatbot/model/encoder.py
+++ chatbot/model/encoder.py
@@ -5,26 +5,44 @@ import torch.nn as nn
 
 
 
-class CustomEncoder(nn.TransformerEncoder):
-    def __init__(self, d_model=512, nhead=8, dim_feedforward=2048, dropout=0.1, 
-                 activation="relu", num_encoder_layers=6, d_memory=2048):
-        
-        encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout, activation)
-        encoder_norm = nn.LayerNorm(d_model)
-        super(CustomEncoder, self).__init__(encoder_layer, num_encoder_layers, encoder_norm)
-        self.vectorizer = nn.Linear(d_model, d_memory)
+class CustomEncoder(torch.nn.Module):
+    def __init__(
+        self,
+        encoder,
+        WM_size : int, 
+        LongTermMemory_size : int, 
+        sentence_emb_dim : int, 
+        max_seq_length : int,
+        ):
+        super().__init__()
+        self.WM_size = WM_size
+        self.sentence_emb_dim = sentence_emb_dim
+        self.encoder = encoder
     
-    def forward(self, src: Tensor, mask: Optional[Tensor] = None, 
-                src_key_padding_mask: Optional[Tensor] = None) -> Tensor:
-        output = src
-
-        for mod in self.layers:
-            output = mod(output, src_mask=mask, src_key_padding_mask=src_key_padding_mask)
-
-        if self.norm is not None:
-            output = self.norm(output) # S,N,E
-
-        output = torch.mean(output, dim=0) # N,E
-        output = self.vectorizer(output) # N, d_memory
-
-        return output
+    def forward(self, x):
+        """
+        input_ids torch.Size([batch, dialogue, seq_len])
+        attention_mask torch.Size([batch, dialogue seq_len])
+        dialogue_mask torch.Size([batch, dialogue])
+        """
+        
+        first_tokens_size=torch.Size((x['input_ids'].size(0), self.WM_size + 2, self.sentence_emb_dim))
+        first_tokens = torch.empty(first_tokens_size)
+         # (batch, dialogue, hidden)
+        
+        action_label = {}
+        for i in range(self.WM_size + 2): # for loop for dialogue length
+            batch = {}
+            for key in x.keys():
+                if key != 'dialogue_mask':
+                    # (batch, dialogue, seq_len).T(1,0) -> (dialogue, batch, seq_len)
+                    # (dialogue, batch, seq_len)[i] -> (batch, seq_len))
+                    batch[key]=x[key].transpose(1,0)[i].type(torch.LongTensor) # (batch,seq)
+            output = self.encoder(**batch) # (batch,seq,sentence_emb_dim)
+            # Allocate sentence vector (first vector of output last_hidden_state) for i th dialogue
+            first_tokens[:,i] = output['last_hidden_state'][:,0,:].detach()
+            
+            if i == self.WM_size:
+                action_label = batch
+        
+        return first_tokens, action_label # (batch, dialogue, hidden), (batch, seq)
